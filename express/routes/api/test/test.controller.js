1238301b64b549f89f1974fd607107f668777779
sbjang <sbjang123456@gmail.com>
2021-04-04 02:35:31 +0900

    test api 기본 구조 정의

---
+++ express/routes/api/test/test.controller.js
@@ -0,0 +1,30 @@
+const Response = require('express/middlewares/Response');
+
+module.exports = {
+    async getTests(req, res) {
+        const { searchText } = req.query;
+        res.json(Response.getSuccessResult({ searchText }));
+    },
+    async postTest(req, res) {
+        const body = req.body;
+        res.json(Response.getSuccessResult({ body }));
+    },
+    async getTest(req, res) {
+        const { id } = req.params;
+        res.json(Response.getSuccessResult({ id }));
+    },
+    async patchTest(req, res) {
+        const { id } = req.params;
+        const body = req.body;
+        res.json(Response.getSuccessResult({ id, body }));
+    },
+    async putTest(req, res) {
+        const { id } = req.params;
+        const body = req.body;
+        res.json(Response.getSuccessResult({ id, body }));
+    },
+    async deleteTest(req, res) {
+        const { id } = req.params;
+        res.json(Response.getSuccessResult({ id }));
+    },
+};
