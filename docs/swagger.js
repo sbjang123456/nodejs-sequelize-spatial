b51ae1c686596e21d9f3b5877a1d6c1c872931da
sbjang <sbjang123456@gmail.com>
2021-04-04 02:22:01 +0900

    test api 추가

--- docs/swagger.js
+++ docs/swagger.js
@@ -5,13 +5,14 @@ const getIp = require('get-ip');
 const swaggerUi = require('swagger-ui-express');
 const { config } = require('../initialize');
 const swaggerDoc = yaml.load(fs.readFileSync(path.join(__dirname, "swaggerV3.yaml"), 'utf8'));
+
 module.exports = app => {
 
     const port = config.comm.nodePort || 8080;
 
     swaggerDoc["servers"] = getIp().map(item => {
         return {
-            url: `http://${item}:${port}/api`
+            url: `http://${item}:${port}/api/v1`
         }
     });
 
--- express/routes/api/index.js
+++ express/routes/api/index.js
@@ -1,26 +1,8 @@
 'use strict';
 
 const router = require('express').Router();
-// const auth = require('./auth');
-// const app = require('./app');
-// const comm = require('./comm');
-// const map = require('./map');
-// const model = require('./model');
-// const project = require('./project');
-// const stats = require('./stats');
-// const task = require('./task');
-// const system = require('./system');
-// const shp = require('./shp');
+const test = require('./test');
 
-// router.use('/auth', auth);
-// router.use('/app', app);
-// router.use('/comm', comm);
-// router.use('/map', map);
-// router.use('/model', model);
-// router.use('/project', project);
-// router.use('/stats', stats);
-// router.use('/task', task);
-// router.use('/system', system);
-// router.use('/shp', shp);
+router.use('/test', test);
 
 module.exports = router;
