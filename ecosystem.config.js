fe145bb5eaed418aebbd30ee647223d890d09444
sbjang <sbjang123456@gmail.com>
2021-04-17 15:27:50 +0900

    babel register 등록

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
     apps: [
         {
             name: 'nodejs-sequelize-spatial', // pm2 name
-            script: './server.js', // server index
-            instances: 1, // 클러스터 모드 사용 시 생성할 인스턴스 수
+            script: './server-register.js', // server index
+            instances: 3, // 클러스터 모드 사용 시 생성할 인스턴스 수
             exec_mode: 'cluster', // fork, cluster 모드 중 선택
             merge_logs: true, // 클러스터 모드 사용 시 각 클러스터에서 생성되는 로그를 한 파일로 합쳐준다.
             autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
